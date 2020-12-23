ef066a7c3ce683d98027f74d07cc6954bf041138
Hatwar Dinesh <hatwar.dinesh@gmail.com>
2020-12-24 04:55:18 +0530

    Added file for addition of two numbers

---
+++ HelloMath.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/cal' , methods=['GET', 'POST'])
+def calculate():
+    try:
+        a = int(request.args.get('a'))
+        b = int(request.args.get('b'))
+        return str(a+b)
+    except ValueError:
+        return "Please enter numeric values only"
+if __name__  == "__main__":
+    app.run()   
