0203bb5485ded7893879750951021a24a3c92db6
Hatwar Dinesh <hatwar.dinesh@gmail.com>
2020-12-24 05:10:55 +0530

    Added code for port change

--- HelloMath.py
+++ HelloMath.py
@@ -12,4 +12,4 @@ def calculate():
     except ValueError:
         return "Please enter numeric values only"
 if __name__  == "__main__":
-    app.run()   
+    app.run(app.run(host='0.0.0.0', port=8080))   
